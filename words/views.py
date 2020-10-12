b822c372a591437311e80d8444c9ff4afbef49e0
KUNAL <you@example.com>
2020-10-12 22:00:03 +0530

    punkit

--- words/views.py
+++ words/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render , HttpResponseRedirect
 import nltk
 nltk.download('wordnet')
 nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
 from nltk.corpus import wordnet
 
 
